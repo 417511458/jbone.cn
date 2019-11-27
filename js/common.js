9205db7873a6db0f384091369063b1588bb9a478
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-27 12:48:23 +0000

    Deploy 417511458/jbone.cn to github.com/417511458/jbone.cn.git:gh-pages

--- js/common.js
+++ js/common.js
@@ -1,14 +1,120 @@
 (function () {
+  initHashLevelRedirects()
   initMobileMenu()
   initVideoModal()
   initNewNavLinks()
   if (PAGE_TYPE) {
+    initVersionSelect()
+    initApiSpecLinks()
     initSubHeaders()
     initLocationHashFuzzyMatching()
   }
 
+  // Most redirects should be specified in Hexo's
+  // _config.yml. However, it can't handle hash-level
+  // redirects, such as:
+  //
+  // /foo#hello -> /bar#hello
+  //
+  // For these cases where a section on one page has
+  // moved to a perhaps differently-named section on
+  // another page, we need this.
+  function initHashLevelRedirects() {
+    checkForHashRedirect(/list\.html$/, {
+      key: '/v2/guide/list.html#维护状态'
+    })
+    checkForHashRedirect(/components\.html$/, {
+      '什么是组件？': '/v2/guide/components.html',
+      '使用组件': '/v2/guide/components-registration.html',
+      '全局注册': '/v2/guide/components-registration.html#全局注册',
+      '局部注册': '/v2/guide/components-registration.html#局部注册',
+      'DOM-模板解析注意事项': '/v2/guide/components.html#解析-DOM-模板时的注意事项',
+      'DOM-模板解析说明': '/v2/guide/components.html#解析-DOM-模板时的注意事项',
+      'data-必须是函数': '/v2/guide/components.html#data-必须是一个函数',
+      '组件组合': '/v2/guide/components.html#组件的组织',
+      'Prop': '/v2/guide/components.html#通过-Prop-向子组件传递数据',
+      'Props': '/v2/guide/components.html#通过-Prop-向子组件传递数据',
+      '使用-Prop-传递数据': '/v2/guide/components.html#通过-Prop-向子组件传递数据',
+      'camelCase-vs-kebab-case': '/v2/guide/components-props.html#Prop-的大小写-camelCase-vs-kebab-case',
+      '动态-Prop': '/v2/guide/components-props.html#静态的和动态的-Prop',
+      '字面量语法-vs-动态语法': '/v2/guide/components-props.html#静态的和动态的-Prop',
+      '单向数据流': '/v2/guide/components-props.html#单向数据流',
+      'Prop-验证': '/v2/guide/components-props.html#Prop-验证',
+      '非-Prop-特性': '/v2/guide/components-props.html#非-Prop-的特性',
+      '替换-合并现有的特性': '/v2/guide/components-props.html#替换-合并已有的特性',
+      '自定义事件': '/v2/guide/components.html#监听子组件事件',
+      '使用-v-on-绑定自定义事件': '/v2/guide/components.html#监听子组件事件',
+      '给组件绑定原生事件': '/v2/guide/components-custom-events.html#将原生事件绑定到组件',
+      'sync-修饰符': '/v2/guide/components-custom-events.html#sync-修饰符',
+      '使用自定义事件的表单输入组件': '/v2/guide/components-custom-events.html#将原生事件绑定到组件',
+      '自定义组件的-v-model': '/v2/guide/components-custom-events.html#自定义组件的-v-model',
+      '在组件上使用-v-model': '/v2/guide/components-custom-events.html#自定义组件的-v-model',
+      '非父子组件的通信': '/v2/guide/state-management.html',
+      '使用插槽分发内容': '/v2/guide/components.html#通过插槽分发内容',
+      '编译作用域': '/v2/guide/components-slots.html#编译作用域',
+      '单个插槽': '/v2/guide/components-slots.html#插槽内容',
+      '具名插槽': '/v2/guide/components-slots.html#具名插槽',
+      '作用域插槽': '/v2/guide/components-slots.html#作用域插槽',
+      '动态组件': '/v2/guide/components.html#动态组件',
+      'keep-alive': '/v2/guide/components-dynamic-async.html#在动态组件上使用-keep-alive',
+      '杂项': '/v2/guide/components-edge-cases.html',
+      '编写可复用组件': '/v2/guide/components.html#组件的组织',
+      '子组件引用': '/v2/guide/components-edge-cases.html#访问子组件实例或子元素',
+      '子组件索引': '/v2/guide/components-edge-cases.html#访问子组件实例或子元素',
+      '异步组件': '/v2/guide/components-dynamic-async.html#异步组件',
+      '高级异步组件': '/v2/guide/components-dynamic-async.html#处理加载状态',
+      '组件命名约定': '/v2/guide/components-registration.html#组件名',
+      '递归组件': '/v2/guide/components-edge-cases.html#递归组件',
+      '组件间的循环引用': '/v2/guide/components-edge-cases.html#组件之间的循环引用',
+      '内联模板': '/v2/guide/components-edge-cases.html#内联模板',
+      'X-Templates': '/v2/guide/components-edge-cases.html#X-Templates',
+      '对低开销的静态组件使用-v-once': '/v2/guide/components-edge-cases.html#通过-v-once-创建低开销的静态组件'
+    })
+    function checkForHashRedirect(pageRegex, redirects) {
+      // Abort if the current page doesn't match the page regex
+      if (!pageRegex.test(window.location.pathname)) return
 
+      var redirectPath = redirects[decodeURIComponent(window.location.hash.slice(1))]
+      if (redirectPath) {
+        window.location.href = window.location.origin + redirectPath
+      }
+    }
+  }
 
+  function initApiSpecLinks () {
+    var apiContent = document.querySelector('.content.api')
+    if (apiContent) {
+      var apiTitles = [].slice.call(apiContent.querySelectorAll('h3'))
+      apiTitles.forEach(function (titleNode) {
+        var methodMatch = titleNode.textContent.match(/^([^(]+)\(/)
+        if (methodMatch) {
+          var idWithoutArguments = slugize(methodMatch[1])
+          titleNode.setAttribute('id', idWithoutArguments)
+          titleNode.querySelector('a').setAttribute('href', '#' + idWithoutArguments)
+        }
+
+        var ulNode = titleNode.parentNode.nextSibling
+        if (ulNode.tagName !== 'UL') {
+          ulNode = ulNode.nextSibling
+          if (!ulNode) return
+        }
+        if (ulNode.tagName === 'UL') {
+          var specNode = document.createElement('li')
+          var specLink = createSourceSearchPath(titleNode.textContent)
+          specNode.innerHTML = '<a href="' + specLink + '" target="_blank" rel="noopener">源代码</a>'
+          ulNode.appendChild(specNode)
+        }
+      })
+    }
+
+    function createSourceSearchPath (query) {
+      query = query
+          .replace(/\([^\)]*?\)/g, '')
+          .replace(/(Vue\.)(\w+)/g, '$1$2" OR "$2')
+          .replace(/vm\./g, 'Vue.prototype.')
+      return 'https://github.com/search?utf8=%E2%9C%93&q=repo%3Avuejs%2Fvue+extension%3Ajs+' + encodeURIComponent('"' + query + '"') + '&type=Code'
+    }
+  }
 
   function parseRawHash (hash) {
     // Remove leading hash
@@ -37,7 +143,7 @@
         normalizedHash = edgeCases[normalizedHash];
       }
       var possibleHashes = [].slice.call(document.querySelectorAll('[id]'))
-        .map(function (el) { return el.id })
+          .map(function (el) { return el.id })
       possibleHashes.sort(function (hashA, hashB) {
         var distanceA = levenshteinDistance(normalizedHash, normalizeHash(hashA))
         var distanceB = levenshteinDistance(normalizedHash, normalizeHash(hashB))
@@ -50,8 +156,8 @@
 
     function normalizeHash (rawHash) {
       return rawHash
-        .toLowerCase()
-        .replace(/\-(?:deprecated|removed|replaced|changed|obsolete)$/, '')
+          .toLowerCase()
+          .replace(/\-(?:deprecated|removed|replaced|changed|obsolete)$/, '')
     }
 
     function levenshteinDistance (a, b) {
@@ -62,10 +168,10 @@
       for (var i = 1; i <= b.length; i++) {
         for (var j = 1; j <= a.length; j++) {
           m[i][j] = b.charAt(i - 1) === a.charAt(j - 1)
-            ? m[i - 1][j - 1]
-            : m[i][j] = Math.min(
-              m[i - 1][j - 1] + 1,
-              Math.min(m[i][j - 1] + 1, m[i - 1][j] + 1))
+              ? m[i - 1][j - 1]
+              : m[i][j] = Math.min(
+                  m[i - 1][j - 1] + 1,
+                  Math.min(m[i][j - 1] + 1, m[i - 1][j] + 1))
         }
       }
       return m[b.length][a.length]
@@ -86,12 +192,12 @@
     ]
     var today = new Date().getTime()
     var updatedLinks = links
-      .filter(function (link) {
-        return link.updatedOn.getTime() + linkExpirePeriod > today
-      })
-      .map(function (link) {
-        return link.title
-      })
+        .filter(function (link) {
+          return link.updatedOn.getTime() + linkExpirePeriod > today
+        })
+        .map(function (link) {
+          return link.title
+        })
 
     var navLinks = document.querySelectorAll('#nav a.nav-link')
     var newLinks = []
@@ -154,8 +260,8 @@
   }
 
   /**
-  * Modal Video Player
-  */
+   * Modal Video Player
+   */
   function initVideoModal () {
     var modalButton = document.getElementById('modal-player')
     var videoModal = document.getElementById('video-modal')
@@ -166,7 +272,7 @@
 
     var videoWrapper = videoModal.querySelector('.video-space')
     var overlay = document.createElement('div')
-        overlay.className = 'overlay'
+    overlay.className = 'overlay'
     var isOpen = false
 
     modalButton.addEventListener('click', function(event) {
@@ -189,7 +295,25 @@
     })
   }
 
+  /**
+   * Doc version select
+   */
 
+  function initVersionSelect () {
+    // version select
+    var versionSelect = document.querySelector('.version-select')
+    versionSelect && versionSelect.addEventListener('change', function (e) {
+      var version = e.target.value
+      var section = window.location.pathname.match(/\/v\d\/(\w+?)\//)[1]
+      if (version === 'SELF') return
+      window.location.assign(
+          'https://' +
+          version +
+          (version && '.') +
+          'vuejs.org/' + section + '/'
+      )
+    })
+  }
 
   /**
    * Sub headers in sidebar
@@ -206,8 +330,8 @@
     var currentPageAnchor = sidebar.querySelector('.sidebar-link.current')
     var contentClasses = document.querySelector('.content').classList
     var isAPIOrStyleGuide = (
-      contentClasses.contains('api') ||
-      contentClasses.contains('style-guide')
+        contentClasses.contains('api') ||
+        contentClasses.contains('style-guide')
     )
     if (currentPageAnchor || isAPIOrStyleGuide) {
       var allHeaders = []
@@ -256,16 +380,16 @@
 
       // make links clickable
       allHeaders
-        .filter(function(el) {
-          if (!el.querySelector('a')) {
-            return false
-          }
-          var demos = [].slice.call(document.querySelectorAll('demo'))
-          return !demos.some(function(demoEl) {
-            return demoEl.contains(el)
+          .filter(function(el) {
+            if (!el.querySelector('a')) {
+              return false
+            }
+            var demos = [].slice.call(document.querySelectorAll('demo'))
+            return !demos.some(function(demoEl) {
+              return demoEl.contains(el)
+            })
           })
-        })
-        .forEach(makeHeaderClickable)
+          .forEach(makeHeaderClickable)
 
       new SmoothScroll('a[href*="#"]', {
         speed: 400,
@@ -323,19 +447,19 @@
         }
       }).join('').replace(/\(.*\)$/, '')
       link.innerHTML =
-        '<a class="section-link" data-scroll href="#' + h.id + '">' +
+          '<a class="section-link" data-scroll href="#' + h.id + '">' +
           htmlEscape(text) +
-        '</a>'
+          '</a>'
       return link
     }
 
     function htmlEscape (text) {
       return text
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
+          .replace(/&/g, '&amp;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#39;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
     }
 
     function collectH3s (h) {
@@ -364,27 +488,27 @@
     function setActive (id, shouldScrollIntoView) {
       var previousActive = sidebar.querySelector('.section-link.active')
       var currentActive = typeof id === 'string'
-        ? sidebar.querySelector('.section-link[href="#' + id + '"]')
-        : id
+          ? sidebar.querySelector('.section-link[href="#' + id + '"]')
+          : id
       if (currentActive !== previousActive) {
         if (previousActive) previousActive.classList.remove('active')
         currentActive.classList.add('active')
         if (shouldScrollIntoView) {
           var currentPageOffset = currentPageAnchor
-            ? currentPageAnchor.offsetTop - 8
-            : 0
+              ? currentPageAnchor.offsetTop - 8
+              : 0
           var currentActiveOffset = currentActive.offsetTop + currentActive.parentNode.clientHeight
           var sidebarHeight = sidebar.clientHeight
           var currentActiveIsInView = (
-            currentActive.offsetTop >= sidebar.scrollTop &&
-            currentActiveOffset <= sidebar.scrollTop + sidebarHeight
+              currentActive.offsetTop >= sidebar.scrollTop &&
+              currentActiveOffset <= sidebar.scrollTop + sidebarHeight
           )
           var linkNotFurtherThanSidebarHeight = currentActiveOffset - currentPageOffset < sidebarHeight
           var newScrollTop = currentActiveIsInView
-            ? sidebar.scrollTop
-            : linkNotFurtherThanSidebarHeight
-              ? currentPageOffset
-              : currentActiveOffset - sidebarHeight
+              ? sidebar.scrollTop
+              : linkNotFurtherThanSidebarHeight
+                  ? currentPageOffset
+                  : currentActiveOffset - sidebarHeight
           sidebar.scrollTop = newScrollTop
         }
       }
@@ -426,14 +550,14 @@
     var escapedSep = escapeRegExp(separator)
 
     var result = str
-      // Remove control characters
-      .replace(rControl, '')
-      // Replace special characters
-      .replace(rSpecial, separator)
-      // Remove continuous separators
-      .replace(new RegExp(escapedSep + '{2,}', 'g'), separator)
-      // Remove prefixing and trailing separators
-      .replace(new RegExp('^' + escapedSep + '+|' + escapedSep + '+$', 'g'), '')
+    // Remove control characters
+        .replace(rControl, '')
+        // Replace special characters
+        .replace(rSpecial, separator)
+        // Remove continuous separators
+        .replace(new RegExp(escapedSep + '{2,}', 'g'), separator)
+        // Remove prefixing and trailing separators
+        .replace(new RegExp('^' + escapedSep + '+|' + escapedSep + '+$', 'g'), '')
 
     switch (options.transform) {
       case 1:
